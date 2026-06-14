a6e50b15544e4687df3a5f35b7fe880d4a4f7c31
Poornima <anupalla00@gmail.com>
2026-06-14 17:58:43 +0530

    Fix CORS

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_origins=[
         "https://levelup-ai-eight.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
